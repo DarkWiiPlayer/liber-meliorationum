103657e387a80377112d69e008896ca5e6333450
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2019-10-01 10:28:08 +0200

    Make rubocop (almost) happy

---
+++ lib/liber_meliorationum.rb
@@ -0,0 +1,207 @@
+# vim: set noexpandtab :miv #
+# frozen_string_literal: true
+
+# A collection of Aliases and Refinements to make life easier
+module LiberMeliorationum
+	# A collection of all the aliases
+	module Alias
+		# Aliases Enumerable#inject as fold
+		module Fold
+			refine Enumerable do
+				alias_method :fold, :inject
+			end
+		end
+		include Fold
+		using Fold
+	end
+	include Alias
+
+	# More easily convert objects to booleans
+	module ToBool
+		refine Object do
+			def to_bool
+				!!self # rubocop:disable Style/DoubleNegation
+			end
+		end
+	end
+	include ToBool
+
+	# Apply a block or proc to an object and return its result
+	module Apply
+		refine Object do
+			using LiberMeliorationum
+			def apply(*args, &function)
+				function.call(self, *args)
+			end
+		end
+	end
+	include Apply
+
+	# Put quotes around a string
+	module Quote
+		refine String do
+			def quote
+				'"' + self + '"'
+			end
+
+			def squote
+				"'" + self + "'"
+			end
+		end
+	end
+	include Quote
+
+	# Assert whether an object is not truthy or a block returns non-truthy
+	module Assert
+		class AssertionFailed < StandardError; end
+		refine Object do
+			using LiberMeliorationum
+			def assert(message = nil, error_class = AssertionFailed, &predicate)
+				raise error_class, message if predicate && !instance_eval(&predicate)
+				raise error_class, message if !predicate && !self
+
+				self
+			end
+
+			def assert_not(message = nil, error_class = AssertionFailed, &predicate)
+				raise error_class, message if predicate && instance_eval(&predicate)
+				raise error_class, message if !predicate && self
+
+				self
+			end
+		end
+	end
+	include Assert
+
+	# Integration of *maybe* monads
+	module Maybe
+		# A maybe monad
+		class Monad < BasicObject
+			attr_reader :object
+			def initialize(object)
+				@object = object
+			end
+
+			def method_missing(sym, *args, &block)
+				if @object
+					Monad.new @object.send(sym, *args, &block)
+				else
+					super or self
+				end
+			end
+
+			def respond_to_missing?(sym)
+				if @object
+					@object.respond_to?(sym) or self
+				else
+					super or self
+				end
+			end
+		end
+
+		refine Object do
+			using LiberMeliorationum
+			def maybe
+				LiberMeliorationum::Maybe::Monad.new(self)
+			end
+		end
+	end
+	include Maybe
+
+	# Allows grouping ennumerables by a given criterion
+	module EnumerableGroupBy
+		refine Enumerable do
+			def group_by(&criterion)
+				each_with_object({}) do |entry, groups|
+					(groups[criterion.call(entry)] ||= []) << entry
+				end
+			end
+		end
+	end
+	include EnumerableGroupBy
+
+	# Adds a *second* method and raising-on-nil versions of
+	# - first
+	# - second
+	# - last
+	module EnumberableNumbered
+		refine Enumerable do
+			using Assert
+			def second
+				self[2]
+			end
+
+			def first!
+				first.assert('Attempting to get first element of empty enumerable')
+			end
+
+			def second!
+				second.assert('Attempting to get second element of empty enumerable')
+			end
+
+			def last!
+				last.assert('Attempting to get last element of empty enumerable')
+			end
+		end
+	end
+	include EnumExtend
+
+	# Adds an `in` method to check whether object is in a collection
+	module In
+		refine Object do
+			def in?(ary)
+				ary.respond_to? :include? and ary.include? self
+			end
+		end
+	end
+	include In
+
+	# Allow creating pipelines from procs
+	module Pipe
+		# Proxy object for a list of procs
+		class Proclist
+			def initialize(*args)
+				@procs = args
+			end
+
+			def +(other)
+				self.class.new(self, other)
+			end
+
+			def <<(other)
+				@procs << other
+				self
+			end
+
+			def call(*args)
+				@procs.each do |procedure|
+					args = [procedure.call(*args)]
+				end
+				args.first
+			end
+
+			def self.<<(arg)
+				new(arg)
+			end
+		end
+
+		refine Proc do
+			def +(other)
+				Proclist.new(self, other)
+			end
+			alias_method :<<, :+
+		end
+	end
+	using Pipe
+
+	# Includes only the more functional modules, leaving out utilities
+	module Core
+		include Alias::Fold
+		include ToBool
+		include Apply
+		include EnumerableGroupBy
+		include Assert
+	end
+end
+
+LiMe = LiberMeliorationum
--- liber_meliorationum.gemspec
+++ liber_meliorationum.gemspec
@@ -1,13 +1,15 @@
 # vim: set noexpandtab :miv #
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
-	s.name        = 'liber-meliorationum'
+	s.name        = 'liber_meliorationum'
 	s.version     = '0.2.0'
-	s.date        = Time::now.strftime '%Y-%m-%d'
+	s.date        = Time.now.strftime '%Y-%m-%d'
 	s.summary     = "Lightweight utility library for Ruby"
 	s.description = "Lightweight utility library for Ruby"
 	s.authors     = ["DarkWiiPlayer"]
 	s.email       = 'darkwiiplayer@hotmail.com'
-	s.files       = ["lib/liber-meliorationum.rb"]
-	s.homepage    = 'http://rubygems.org/gems/liber-meliorationum'
+	s.files       = ["lib/liber_meliorationum.rb"]
+	s.homepage    = 'http://rubygems.org/gems/liber_meliorationum'
 	s.license     = 'MIT'
 end
