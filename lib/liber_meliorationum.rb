5c7106358f9e4d06ca59db9bb1a7c338f913595d
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-07-08 12:55:21 +0200

    Restructure and Refactor

--- lib/liber_meliorationum.rb
+++ lib/liber_meliorationum.rb
@@ -1,35 +1,26 @@
-# vim: set noexpandtab :miv #
 # frozen_string_literal: true
 
 # A collection of Aliases and Refinements to make life easier
 module LiberMeliorationum
-	# A collection of all the aliases
-	module Alias
-		# Aliases Enumerable#inject as fold
-		module Fold
-			refine Enumerable do
-				alias_method :fold, :inject
-			end
-		end
-		include Fold
-		using Fold
-	end
-	include Alias
-
-	# More easily convert objects to booleans
+	# Less esoteric way to convert any value into a boolean
+	# 	using LiMe::ToBool
+	# 	puts 3.to_bool
 	module ToBool
 		refine Object do
 			def to_bool
-				!!self # rubocop:disable Style/DoubleNegation
+				!!self
 			end
 		end
 	end
 	include ToBool
 
 	# Apply a block or proc to an object and return its result
+	# using LiMe::Apply
+	# 	10.apply(1, {|number, other| number + other})
+	# 	#or:
+	# 	10.apply(1, &:+)
 	module Apply
 		refine Object do
-			using LiberMeliorationum
 			def apply(*args, &function)
 				function.call(self, *args)
 			end
@@ -37,7 +28,11 @@ module LiberMeliorationum
 	end
 	include Apply
 
-	# Put quotes around a string
+	# Surrounds a string with quotes.
+	# No escaping is done if the string contains quotes already.
+	# 	using LiMe::Quote
+	# 	puts "Hello, World!".quote
+	# 	puts "Hello, World!".squote
 	module Quote
 		refine String do
 			def quote
@@ -51,122 +46,115 @@ module LiberMeliorationum
 	end
 	include Quote
 
-	# Assert whether an object is not truthy or a block returns non-truthy
+	# Assert whether an object is not truthy or a block returns non-truthy.
+	# Returns the value unmodified.
+	# 	using LiMe::Assert
+	# 	user = nil
+	# 	user.assert("Expected user to exist")
+	# 	20.assert("Number must be less than 10") { |num| num < 10 }
 	module Assert
 		class AssertionFailed < StandardError; end
 		refine Object do
-			using LiberMeliorationum
-			def assert(message = nil, error_class = AssertionFailed, &predicate)
-				raise error_class, message if predicate && !instance_eval(&predicate)
-				raise error_class, message if !predicate && !self
-
+			def assert(message = nil, error_class = AssertionFailed)
+				raise error_class, message if block_given? && !yield(self)
+				raise error_class, message if !block_given? && !self
 				self
 			end
 
-			def assert_not(message = nil, error_class = AssertionFailed, &predicate)
-				raise error_class, message if predicate && instance_eval(&predicate)
-				raise error_class, message if !predicate && self
-
+			def assert_not(message = nil, error_class = AssertionFailed)
+				raise error_class, message if block_given? && yield(self)
+				raise error_class, message if !block_given? && self
 				self
 			end
 		end
 	end
 	include Assert
 
-	# Make sure an array only has one element and return it
-	module Value
-		refine Object do
-			def value
-				self
-			end
-		end
+	# Make sure an array or hash only has one element and return it.
+	# 	users = [ User.new ]
+	# 	user = users.only
+	# 	users = { steve: User.new(name: 'Steve') }
+	# 	user = hash.only
+	module Only
 		refine Hash do
-			using Assert
-			def value
-				assert("Expected hash to have exactly one pair"){ keys.size == 1 }
+			def only
+				raise RuntimeError, "Expected hash to have exactly one pair" unless keys.size == 1
 				self[keys.first]
 			end
 		end
 		refine Array do
-			using Assert
-			def value
-				assert("Expected array to have exactly one element"){ size == 1 }
+			def only
+				raise RuntimeError, "Expected array to have exactly one value" unless length == 1
 				first
 			end
 		end
 	end
-	include Value
+	include Only
 
-	# Integration of *maybe* monads
-	module Maybe
-		# A maybe monad
-		class Monad < BasicObject
-			attr_reader :object
-			def initialize(object)
-				@object = object
-			end
-
-			def method_missing(sym, *args, &block)
-				Maybe::Monad.new @object.send(sym, *args, &block) rescue Maybe::Monad.new nil
-			end
-
-			def respond_to_missing?(sym)
-				if @object
-					@object.respond_to?(sym) or super
-				else
-					super
-				end
-			end
-		end
-
-		refine Object do
-			using LiberMeliorationum
-			def maybe
-				LiberMeliorationum::Maybe::Monad.new(self)
-			end
-		end
-	end
-	include Maybe
-
-	# Allows grouping ennumerables by a given criterion
-	module EnumerableGroupBy
-		refine Enumerable do
-			def group_by(&criterion)
-				each_with_object({}) do |entry, groups|
-					(groups[criterion.call(entry)] ||= []) << entry
+	# Sorts elements into a tree according to a list of functions.
+	# This is essentially a nested version of Array#group_by.
+	# 	using LiMe::TreeBy
+	# 	Tester = Struct.new(:foo, :bar, :baz, :name)
+	# 	[
+	# 		Tester.new(:a, :a, :a, "first"),
+	# 		Tester.new(:a, :b, :a, "second"),
+	# 		Tester.new(:b, :a, :a, "third"),
+	# 	].tree_by(&:a, &:b, &:c)
+	# This will split the elements as follows:
+	# Hash
+	# ├──:a
+	# │  ├──:a
+	# │  │  └──:a => first
+	# │  └──:b
+	# │     └──:a => second
+	# └──:b
+	#    └──:a
+	#       └──:a => third
+	module TreeBy
+		refine Array do
+			def tree_by(criterion, *criteria)
+				group_by(&criterion.to_proc).then do |result|
+					if criteria.empty?
+						result
+					else
+						result.map{ |key, ary| [key, ary.tree_by(*criteria)] }.to_h
+					end
 				end
 			end
 		end
 	end
-	include EnumerableGroupBy
+	include TreeBy
 
 	# Adds a *second* method and raising-on-nil versions of
 	# - first
 	# - second
 	# - last
-	module EnumerableNumbered
-		refine Enumerable do
-			using Assert
+	# 	[:foo].second # returns nil
+	# 	[:foo].second! # raises an error
+	module Numbered
+		refine Array do
 			def second
 				self[2]
 			end
 
-			def first!
-				first.assert('Attempting to get first element of empty enumerable')
-			end
-
-			def second!
-				second.assert('Attempting to get second element of empty enumerable')
-			end
-
-			def last!
-				last.assert('Attempting to get last element of empty enumerable')
-			end
-		end
+#			def first!
+#				# TODO: Implement again
+#			end
+#
+#			def second!
+#				# TODO: Implement again
+#			end
+#
+#			def last!
+#				# TODO: Implement again
+#			end
+#		end
 	end
 	include EnumerableNumbered
 
-	# Adds an `in` method to check whether object is in a collection
+	# Adds an `in` method to check whether object is in a collection.
+	# This works on everything that responds to :include?
+	# 	3.in? (1..10)
 	module In
 		refine Object do
 			def in?(ary)
@@ -176,53 +164,54 @@ module LiberMeliorationum
 	end
 	include In
 
-	# Allow creating pipelines from procs
-	module Pipe
-		# Proxy object for a list of procs
-		class Proclist
-			def initialize(*args)
-				@procs = args
+	# Adds an `if` method to conditionally transform an object
+	# Boolean and Block
+	# 	20.if(true){|num| num+1} # 21
+	# Boolean and chained method call
+	# 	20.if(true).next # 21
+	# 	20.if(false).next # 20
+	# Block and chained method call
+	# 	20.if{|num| num < 40}.next #21
+	# 	20.if{|num| num < 10}.next #20
+	module If
+		class Skip < BasicObject
+			def initialize object
+				@object = object
 			end
-
-			def +(other)
-				self.class.new(self, other)
+			def method_missing name, *args, **params
+				return @object
 			end
+		end
 
-			def <<(other)
-				@procs << other
-				self
-			end
+		MISSING = Object.new
 
-			def call(*args)
-				@procs.each do |procedure|
-					args = [procedure.call(*args)]
+		refine Object do
+			def if bool=MISSING
+				if bool==MISSING
+					if yield self
+						self
+					else
+						Skip.new(self)
+					end
+				else
+					if block_given?
+						if bool
+							yield self
+						else
+							self
+						end
+					else
+						if bool
+							self
+						else
+							Skip.new(self)
+						end
+					end
 				end
-				args.first
-			end
-
-			def self.<<(arg)
-				new(arg)
 			end
 		end
-
-		refine Proc do
-			def +(other)
-				Proclist.new(self, other)
-			end
-			alias_method :<<, :+
-		end
 	end
-	using Pipe
-
-	# Includes only the more functional modules, leaving out utilities
-	module Core
-		include Alias::Fold
-		include ToBool
-		include Apply
-		include EnumerableGroupBy
-		include Assert
-		include Value
-	end
+	include If
 end
 
 LiMe = LiberMeliorationum
--- readme.md
+++ readme.md
@@ -1,89 +1,23 @@
 <!-- vim: set noexpandtab tabstop=3 :miv -->
 
-Liber Meliorationum
-================================================================================
+# Liber Meliorationum
 
-This is a simple library that aims to make daily tasks in Ruby easier by providing refinements and aliases.
+A collection of general-purpose ruby refinements that aim to make everyday tasks
+easier.
 
-Structure
---------------------------------------------------------------------------------
+Examples for what everything does can be found in the code, but the code itself
+should be easy enough to understand too.
 
--	Alias
-	-	Fold
-		adds a `fold` alias to `Enumerable#inject`.
--	ToBool
-	adds a `to_bool` method to `Object` to avoid cryptic idioms like `!!object`.
--	Apply
-	adds an `apply` method to `Object`,
-	which evaluates a given block in the context of the object.
-	This allows using Procs and Lambdas in a more point-free, object oriented style.
--	Maybe
-	adds a *maybe* monad and a method to `object` to convert it into one.
--	Assert
-	adds an `assert` method to `Object` which behaves similar to Luas `assert` function.
-	It raises an error with the provided message when the provided block returns `false`.
-	Otherwise returns the object itself.
--	Enumerable#group\_by(&criterion)
-	Filters through the elements of an Enumerable. Calls `criterion` on every
-	element and groups elements by what `criterion` returns.
-	Returns a hash mapping criterion(element) => [element, element, ...]
--	EnumerableNumbered
-	Adds Enumerable#second method (works as expected)
-	and Enumerable#first!, #second! and #last!, which error instead of returning nil
--	In
-	adds an `in? ary` method to Object which checks if the array occurs as an
-	element of `ary`. If `ary` does not respond to `include?`, `false` is
-	automatically returned.
--	Value
-	For objects, returns the object itself.
-	For arrays, returns the single value of an array or raises if the array has
-	zero or more than one elements.
-	For hashes, acts like for arrays, but returns the value.
--	Pipe (⚠️ Experimental)
-	Allows "adding" procs to each other to chain them.
-	`(a + b).call(foo)` is the same as `(b.call(a.call(foo)))`
-	The `<<` operation does the same, but without creating a new object every
-	time, so it's more efficient for one-liners.
+## A few selected examples
 
-Examples
---------------------------------------------------------------------------------
+Many of the refinements add helpers to make method chaining easier:
 
-	using LiMe # using LiberMeliorationum
-
-	# LiMe::Alias::Fold
-	puts [1, 2, 3, 4, 5].fold {|a, b| a+b} # prints 15
-
-	# LiMe::ToBool
-	puts nil.to_bool.to_s # prints false
-
-	# LiMe::Apply
-	puts "hello".apply { |str| str.upcase } # prints HELLO
-
-	# LiMe::Maybe
-	puts [].first.even? # raises an error
-	puts [].maybe.first.even?.value # prints nil
-
-	# LiMe::Assert
-	puts 20.assert('Number is not even', &:even?).to_s # prints 20
-	puts 19.assert('Number is not even', &:even?).to_s # Raises an error
-
-	# LiMe::EnumerableGroupBy
-	[1, 2, 3, 4].group_by(&:even?) # returns {true => [2,4], false => [1,3]}
-
-	# LiMe::EnumerableNumbered
-	[1, 2].second # returns 2
-	[1, 2].last   # returns 2
-	[1].first!    # returns 1
-	[].first!     # raises an error
-
-	# LiMe::In
-	20.in? [1, 2, 3] # returns false
-
-	# LiMe::Value
-	'foo'.value   # returns 'foo'
-	['foo'].value # returns 'foo'
-	[].value      # raises an error
-	[1, 2].value  # raises an error
-	{a: 'b'}      # returns 'b'
-
-	puts 'Hello'.quote # prints "Hello"
+	user = User.first
+		# Raise an error on missing user without breaking the method chain
+		.assert("No users found")
+		.assert("User has been disabled", &:disabled?)
+		.assert("User is not old enough") { |user| user.age >= 18 }
+		# Execute a single method on a condition
+		.if(&:enabled?).disable!
+		# Works well with Ruby's `tap` and `then` methods
+		.if{|user| user.disabled?}.tap{|user| user.enable}
